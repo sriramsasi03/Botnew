322b25164379ca70c8c6e5f978ea38907f2bfa1c
sriramsasi03 <sriramsasiphani1@gmail.com>
2021-03-22 10:26:35 +0530

    wrote the intrest code

--- index.js
+++ index.js
@@ -6,11 +6,28 @@ bot.once('ready', () => {
 	console.log('Ready!');
 });
 
+
 bot.on('message', msg => {
-	if (msg.content === 'ping') {
-    
-		msg.channel.send('Pong.');
+	if (msg.author.bot) return;
+ 	if (!msg.content.startsWith("!")) return;
+	const commandBody = msg.content.slice("!".length);
+	const args = commandBody.split(' ');
+	const command = args.shift().toLowerCase();
+  
+	if (command === "ping") {
+	  const timeTaken = Date.now() - msg.createdTimestamp;
+	  msg.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+	}
+  
+	else if (command === "intrest") {
+	  const numArgs = args.map(x => parseFloat(x));
+	  const sum = numArgs.reduce((counter, x) => counter *= x);
+	  msg.reply(`The Intrest you get is $${sum/100}!`);
+	}
+	else if(command ==="help") {
+		msg.reply("YOU NEED TO GIVE THE 3 INPUTS FOR CORRECT RESULTS")
 	}
 });
+
 
 bot.login(config.TOKEN);
